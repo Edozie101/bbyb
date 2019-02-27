a6faee251191843a464edc4bb623b35ce822b761
edzye101 <edzye101@gmail.com>
2019-02-27 15:28:07 +0000

    added User and Property

--- bbb/bricks/models.py
+++ bbb/bricks/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+#import models from django.db 
+
+#unlike RoR migrations are made directly from the models 
+
+from django.db import models
+
+#Property Model
+class Property(models.Model):
+	road = models.CharField(max_length = 250)
+	town = models.CharField(max_length = 250)
+	postcode = models.CharField(max_length = 10)
+	description = models.CharField(max_length = 10000)
+	features = models.CharField(max_length = 2000)
+	pub_date = models.DateTimeField('date published')
+	image = models.ImageField
+
+
+
+# Stipe or Payment provider 
+
+
+
+
+# User model
+
+
+class User(models.Model):
+	first_name = models.CharField(max_length = 50)
+	last_name = models.CharField(max_length = 50)
+	email= models.CharField(max_length = 100)
+	postcode = models.CharField(max_length = 10)
+	address = models.CharField(max_length = 400)
+	password = models.CharField(max_length = 250)
+	
