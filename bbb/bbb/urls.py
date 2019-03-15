e8ca660ce8a161b0af1890dcb1f63a6f5b08edab
edzye101 <edzye101@gmail.com>
2019-03-15 15:08:59 +0000

    pushing new oriented website

--- bbb/bbb/urls.py
+++ bbb/bbb/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bricks/', include('bricks.urls')),
+    path('', include('bricks.urls')),
 ]
