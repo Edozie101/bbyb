6bccf79482ad16c6fa45c2468869af765659b0f8
edzye101 <edzye101@gmail.com>
2019-04-17 13:28:46 +0100

    adding the property information

--- bbb/bricks/urls.py
+++ bbb/bricks/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import staticfiles
 from django.contrib.staticfiles.urls import static
 #import views from root directory of project
 from . import views
